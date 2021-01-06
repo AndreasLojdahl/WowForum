edc7813259c7bc0f83e5b28d1e4701e1c85a459f
andreasLojdahl <andreas.lojdahl@gmail.com>
2021-01-06 19:47:03 +0100

    started working on a admin page for features like promote a user to a moderator and delete user if you are logged in as a admin

---
+++ src/main/resources/frontend/src/components/UserListItem.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <div class="col">{{ user.username }}</div>
+        <div class="col">{{ user.email }}</div>
+      </div>
+      <div>{{user.roles}}</div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "UserListItem",
+  props: {
+    user: Object,
+  },
+};
+</script>
+<style scoped></style>
---
+++ src/main/resources/frontend/src/views/AdminPage.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container">
+    <form v-if="user" @submit.prevent="createThread" class="mb-4">
+      <div class="form-group">
+        <label for="username">Användarnamn</label>
+        <input
+          type="text"
+          class="form-control"
+          id="username"
+          required
+          v-model="username"
+        />
+      </div>
+      <button type="submit" class="btn btn-dark">
+        Sök användare
+      </button>
+    </form>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      username: null
+    };
+  },
+};
+</script>
+<style scoped></style>
